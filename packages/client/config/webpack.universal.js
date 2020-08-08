c81ad70ed421da58e9364d0ab551acd98ddece6d
Bowen Liu <mr_lbw@outlook.com>
2020-08-08 23:09:56 +0800

    feat: bootstrap

---
+++ packages/client/config/webpack.universal.js
@@ -0,0 +1,33 @@
+const path = require(`path`)
+const webpack = require(`webpack`)
+
+const __DEV__ = process.env.NODE_ENV === `development`
+
+function resolveApp(p) {
+  return path.resolve(__dirname, `..`, p)
+}
+
+const universal = {
+  mode: __DEV__ ? `development` : `production`,
+
+  output: {
+    path: resolveApp(`../dist`),
+    publicPath: `/public/`,
+    filename: `[name].[${__DEV__ ? `hash` : `contenthash`}:8].js`,
+  },
+
+  module: {},
+
+  plugins: [
+    new webpack.DefinePlugin({
+      __DEV__: JSON.stringify(__DEV__),
+      __VERSION__: JSON.stringify(require(`../package.json`).version),
+    }),
+  ],
+
+  resolve: {
+    extensions: [`.ts`, `.tsx`],
+  },
+}
+
+module.exports = universal
