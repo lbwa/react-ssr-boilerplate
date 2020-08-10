4f73568073cefb1efb552faeb003c625d74f54c4
Bowen Liu <mr_lbw@outlook.com>
2020-08-10 18:26:07 +0800

    feat(client): impl ssr client

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,7 +1,30 @@
 import React from 'react'
 
 const App: React.FC = () => {
-  return <div>React application</div>
+  return (
+    <>
+      <h1>react.js ssr boilerplate</h1>
+      <section onClick={() => console.log(`clicked !`)}>
+        <h3>available global env variables</h3>
+        <ul>
+          <li>
+            <label>__DEV__</label>
+            <span>{JSON.stringify(__DEV__)}</span>
+          </li>
+        </ul>
+      </section>
+      <footer>
+        Created&nbsp;by&nbsp;
+        <a
+          href="https://github.com/lbwa"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Bowen Liu
+        </a>
+      </footer>
+    </>
+  )
 }
 
 export default App
